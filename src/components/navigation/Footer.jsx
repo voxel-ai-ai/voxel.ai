2dfccf00cef4a888b81d479b4670911aa459d4c7
Ai Workshop <aiworkshop965@gmail.com>
2026-03-05 21:14:30 +0000

    File changes

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import VoxelLogo from '../VoxelLogo';
