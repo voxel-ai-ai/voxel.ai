96a0224681326e0074a6dcfa36f5220df1d69ba8
Ai Workshop <aiworkshop965@gmail.com>
2026-03-17 20:22:46 +0000

    File changes

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -42,6 +42,7 @@ export default function Navbar() {
   };
 
   return (
+    <>
     <nav 
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
         scrolled ? 'border-b border-border' : ''
